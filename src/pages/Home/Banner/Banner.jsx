dc0683c2946fe0586b845e40efa48777f56ce056
dev-ashrafuzzaman <dev.ashrafuzzaman@gmail.com>
2023-06-09 21:50:34 +0600

    Finally Complete Toy Marketplace project

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,7 +3,7 @@ const Banner = () => {
     return (
         <div className="container mx-auto md:p-10 p-2 relative ">
             <div>
-                <img className="rounded-lg border border-[#7194BE]" src="https://i.ibb.co/kq9GTXJ/fotor-2023-6-8-12-0-15.png" alt="" />
+                <img className="rounded-lg  border-4 border-[#F379A7]" src="https://i.ibb.co/kq9GTXJ/fotor-2023-6-8-12-0-15.png" alt="" />
             </div>
         </div>
     );
--- src/pages/Home/CategorySection/CategorySection.jsx
+++ src/pages/Home/CategorySection/CategorySection.jsx
@@ -1,9 +1,8 @@
 
-
 const CategorySection = () => {
     return (
-        <div className="container mx-auto md:px-28 md:top-[350px] md:left-[90px] lg:px-36 lg:top-[450px] lg:left-[100px]  xl:px-40 xl:top-[550px] xl:left-[10px]  2xl:px-80 2xl:top-[650px] 2xl:left-[200px] md:absolute p-2 md:p-0 ">
-            <div className="flex justify-evenly text-center md:font-bold text-[10px] md:text-xl text-[#DB83A6] gap-10 items-center shadow-xl bg-white md:rounded-full rounded-lg p-4">
+        <div className="container mx-auto  md:px-28 md:top-[350px] md:left-[90px] lg:px-36 lg:top-[450px] lg:left-[100px]  xl:px-40 xl:top-[550px] xl:left-[10px]  2xl:px-80 2xl:top-[650px] 2xl:left-[200px] md:absolute p-2 md:p-0 ">
+            <div className="border border-[#F379A7] flex justify-evenly text-center md:font-bold text-[10px] md:text-xl text-[#DB83A6] gap-10 items-center shadow-xl bg-white md:rounded-full rounded-lg p-4">
                 <div className="hover:text-[#57C4E4]">
                     <img className="w-[100px]" src="https://i.ibb.co/fq8CsYg/fotor-2023-6-8-12-17-49.png" alt="" />
                     <p>Sports car</p>
